598de444b85d0d37094fb596ef483c1a94a033b0
Brian Broll <brian.broll@gmail.com>
2015-04-11 14:54:37 -0500

    Added structure for server tests

---
+++ test/server.js
@@ -0,0 +1,9 @@
+/*globals describe,it*/
+'use strict';
+
+describe('Server API tests', function() {
+    it('Should return restaurants given a places request', function(done) {
+        // Add server test
+        // TODO
+    });
+});
