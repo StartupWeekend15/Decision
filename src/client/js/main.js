845cbc83e9a60da77ec91daa9d25c506fd72460d
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2015-04-12 12:14:59 -0500

    Add recommendation page

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -35,6 +35,11 @@ var getCategoryMap = function() {
 require.config({
     paths: {
         jquery: './lib/jquery-2.1.3.min'
+    },
+    map: {
+        '*': {
+            'text': './lib/require-text/text'
+        }
     }
 });
 
