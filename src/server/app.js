c5cbf074e2a5592b91d709906761714270f1d2e9
Brian Broll <brian.broll@gmail.com>
2015-04-16 10:15:30 -0500

    Added TestRequestor to default for dev environment

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,8 @@
 var Server = require('./Server'),
     opts = {
+        requestor: process.env.NODE_ENV === 'development'? 
+                   'Test' :
+                   'Google',
         port: this._port = process.env.PORT || 3700,
         apiKey: process.env.GOOGLE_PLACES_API_KEY || 'AIzaSyDIw6RsLMhU2Z3WRNDpWlx3iOGJjfRFkj4'  // FIXME
     },
