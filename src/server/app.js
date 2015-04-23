d653db4331131f4cc98995dfdf21c945adb334b3
Brian Broll <brian.broll@gmail.com>
2015-04-23 07:32:40 -0500

    Moved API key to environment variable.
    In order to update your local copy, create a file called ".env" and add GOOGLE_PLACES_API_KEY=<API_KEY>

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,19 @@
 
 var Server = require('./Server'),
-    opts = {
-        requestor: process.env.NODE_ENV === 'development'? 
-                   'Test' :
-                   'Google',
-        port: this._port = process.env.PORT || 3700,
-        apiKey: process.env.GOOGLE_PLACES_API_KEY || 'AIzaSyDIw6RsLMhU2Z3WRNDpWlx3iOGJjfRFkj4'  // FIXME
-    },
-    server = new Server(opts);
+    dotenv = require('dotenv'),
+    server,
+    opts;
 
+dotenv.load();
+
+opts = {
+    requestor: process.env.NODE_ENV === 'development'? 
+               'Test' :
+               'Google',
+    port: this._port = process.env.PORT || 3700,
+    apiKey: process.env.GOOGLE_PLACES_API_KEY 
+};
+
+server = new Server(opts);
 server.start();
 
