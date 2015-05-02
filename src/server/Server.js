fc2491ac9a9ffae8a65b6e517894ab60c3a19113
Kate Brady <kate@Kates-MacBook.local>
2015-05-02 15:05:22 -0500

    Change the values returned for each result

--- src/server/Server.js
+++ src/server/Server.js
@@ -133,12 +133,32 @@ Server.prototype.getPlaces = function(params, callback) {
 /**
  * Convert a single request
  *
+ * vicinity = simplified address (pass directly to google maps on client side)
+ * types = can take one of the following values:
+ *   accounting airport amusement_park aquarium art_gallery atm bakery bank bar beauty_salon bicycle_store book_store bowling_alley bus_station
+ *   cafe campground car_dealer car_rental car_repair car_wash casino cemetery church city_hall clothing_store convenience_store courthouse dentist
+ *   department_store doctor electrician electronics_store embassy establishment finance fire_station florist food funeral_home furniture_store gas_station
+ *   general_contractor grocery_or_supermarket gym hair_care hardware_store health hindu_temple home_goods_store hospital insurance_agency jewelry_store
+ *   laundry lawyer library liquor_store local_government_office locksmith lodging meal_delivery meal_takeaway mosque movie_rental movie_theater moving_company
+ *   museum night_club painter park parking pet_store pharmacy physiotherapist place_of_worship plumber police post_office real_estate_agency restaurant
+ *   roofing_contractor rv_park school shoe_store shopping_mall spa stadium storage store subway_station synagogue taxi_stand train_station travel_agency
+ *   university veterinary_care zoo
+ * location = contains the geocoded latitude,longitude value for this place.
+ * formatted_phone_number = phone number locally formatted
+ * rating = place rating
+ * price_level = number 0 to 4 representing price
+ * website = lists the authoritative website for this place, such as a business' homepage
+ *
  * @param result
  * @return {undefined}
  */
 Server.prototype.convertResult = function(result) {
-    var fields = ['name', 'icon', 'rating', 'vicinity', 'opening_hours', 'price_level'];
-    return R.pick(fields, result);
+    var fields = ['place_id', 'name', 'icon', 'vicinity', 'types', 'geometry', 'formatted_phone_number', 'rating', 'price_level', 'website'];
+    var json_result = R.pick(fields, result);
+    json_result['location'] = json_result['geometry']['location'];
+    delete json_result.geometry;
+    console.log(json_result);
+    return json_result;
 };
 
 Server.prototype.start = function(callback) {
