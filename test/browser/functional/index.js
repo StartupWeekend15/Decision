935b66546a7615211cd20cb047157c887c35abb6
Brian Broll <brian.broll@vanderbilt.edu>
2015-05-05 15:04:07 -0500

    Added food options tests

--- test/browser/functional/index.js
+++ test/browser/functional/index.js
@@ -32,4 +32,40 @@ define([
                     });
             });
         });
+
+        bdd.describe('Food', function() {
+            bdd.it('should load first food option', function() {
+                return this.remote
+                    .get('http://localhost:'+port)  // Load the page
+                    .setFindTimeout(10000)  // wait 10 secs for it to load
+                    .findByCssSelector('#foodButton')  // Find the food button
+                    .click()  // Click the food button
+                    .end()  // stop the click event
+                    .findByCssSelector('.result__vicinity')  // find an element in the results page
+                    .isDisplayed()  // Check if .result_vicinity is displayed
+                    .then(function(displayed) {  // load the isDisplayed result
+                        assert(displayed, 'First option not loaded');
+                    });
+            });
+
+            bdd.it('should load 2 food options', function() {
+                return this.remote
+                    .get('http://localhost:'+port)
+                    .setFindTimeout(10000)
+                    .findByCssSelector('#foodButton')
+                    .click()
+                    .end()
+                    .setFindTimeout(10000)
+                    // "Next" button
+                    .findByCssSelector('#nextButton')
+                    .click()
+                    .end()
+
+                    .findByCssSelector('.result__vicinity')
+                    .isDisplayed()
+                    .then(function(displayed) {
+                        assert(displayed, 'First option not loaded');
+                    });
+            });
+        });
     });
